7a60e65e2d983e1c0d28ce9545a001967b405a43
Arturo Wibawa <agwibawa@gmail.com>
2021-03-30 11:37:12 -0700

    server js added

--- components/AnimationVariants.js
+++ components/AnimationVariants.js
@@ -91,11 +91,13 @@ export const variants = {
   entryItem: {
     initial: {
       opacity: 0,
-      scale: 0.8,
+      // scale: 0.8,
+      x: 100,
     },
     enter: {
       opacity: 1,
-      scale: 1,
+      // scale: 1,
+      x: 0,
       transition: {
         type: 'spring',
         stiffness: 600,
