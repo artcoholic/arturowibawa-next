cb776984e34435d8f2f32b9f27716aea2f22826e
Arturo Wibawa <agwibawa@gmail.com>
2021-05-04 22:56:00 -0700

    404 page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,14 @@
+import Link from 'next/link';
 import Box from '../components/Box';
 import Text from '../components/Text';
 
 export default function Custom404() {
   return (
-    <Box width="100vw" height="100vh" display="flex" justifyContent="center" alignItems="center">
-      <Text px={12} py={8} bg="bg.secondary">404 - Page Not Found</Text>
+    <Box width="100vw" height="100vh" display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+      <Text px={12} py={8} mb={12} bg="bg.secondary">404 - Page Not Found</Text>
+      <Link href="/" passHref>
+        <Text as="a">Go back home</Text>
+      </Link>
     </Box>
   )
 }
